03936d76707c347dc660131d84ec1e6f77b9ea34
alexanderArsenault <rsenaultc@gmail.com>
2021-03-08 01:00:36 +0100

    added stream

--- static/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const environment = process.env.NODE_ENV
-
-module.exports = environment => {
-  env = !!environment.prod ? 'production' : 'development'
-  
-  console.log(`🛠️  running ${env} Mode using ./webpack/webpack.${env}.config.js 🛠️`);
-  return require(`./webpack/webpack.${env}.config.js`);
-};
